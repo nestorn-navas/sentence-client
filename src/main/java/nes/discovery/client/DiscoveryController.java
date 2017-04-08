4796d36c74e094cacceebb11e2463eba03e005c1
Nestor Navas <nestor.navas@gmail.com>
2017-04-08 14:11:15 +0200

    STS update

---
+++ src/main/java/nes/discovery/client/DiscoveryController.java
@@ -0,0 +1,31 @@
+package nes.discovery.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class DiscoveryController {
+	
+	@Autowired
+	private LoadBalancerClient balancer;
+	
+	@RequestMapping("/sentence")
+	public @ResponseBody String getSentence() {
+		return getWord("SUBJECT") + " " + 
+	           getWord("VERB") + " " + 
+			   getWord("ARTICLE") + " " + 
+	           getWord("ADJECTIVE") + " " + 
+			   getWord("NOUN") + ".";
+	}
+
+	public String getWord(String service) {
+		ServiceInstance instance = balancer.choose(service);
+   		return (new RestTemplate()).getForObject(instance.getUri(),String.class);
+	}
+
+}
